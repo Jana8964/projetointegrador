d64e2fcf22cb8667c183dac25c3cc6a23f316495
jana8964e <janaise.g@escolar.ifrn.edu.br>
2021-04-26 02:08:06 -0300

    outra

---
+++ application/views/novo_cadastro.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form>
+        Nome: <input type= "text" name= "nome"/><br/>
+        Matrícula: <input type= "number" name= "numeros"/><br/>
+
+        <button type= "submit"> Enviar </button>
+        <button type= "resert"> Limpar </button>
+    </form>    
+
+</body>
+</html>
